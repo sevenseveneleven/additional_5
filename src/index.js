0ebd7e8755c57ea877c12555d292fb3e815f5544
aaa <it-z@tut.by>
2018-03-05 16:42:40 +0300

    just for now

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+   // your solution
+ //return true;
+  var stack = [];
+  for (i = 0; i < str.length; i++) {
+    for (j = 0; j < bracketsConfig.length; j++) {
+      
+     if (str[i] == bracketsConfig[j][0]) {
+			stack.push(str[i]);
+     }
+     
+     if (str[i] == bracketsConfig[j][1]) {
+       
+       if (stack[stack.length-1] == bracketsConfig[j][0] && stack.length != 0) {
+         stack.pop();
+       
+       } else {
+                 return false;
+              }
+     }
+    }
+  }
+  if (stack.length == 0) {
+	  return true;
+	  } else {
+		  return false;
+	  }
 }
